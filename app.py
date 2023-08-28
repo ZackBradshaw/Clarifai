caeea938b79b53a358216b7eb5ceeccc1e7257db
Zack <zack@zackbradshaw.com>
2023-08-28 05:03:38 -0500

    remove agent memory

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from langchain.llms import Clarifai
 import streamlit as st
 from langchain import PromptTemplate, LLMChain
-from agentmemory import create_memory, search_memory
 
 # Define the ProfitPilot class
 def __init__(self, ai_name, ai_role, external_tools, company_name, company_values, conversation_type, conversation_purpose, company_business, salesperson_name, human_in_the_loop, prospect_name):
@@ -42,7 +41,7 @@ AI_ROLE = f"""
         If you're asked about where you got the user's contact information, say that you got it from public records.
         Keep your responses in short length to retain the user's attention. Never produce lists, just answers.
         Start the conversation by just a greeting and how is the prospect doing without pitching in your first turn.
-        When the conversation is over, output <END_OF_CALL>
+        When the conversation is over, output 
         Always think about at which conversation stage you are at before answering:
 
         1: Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming. Always clarify in your greeting the reason why you are calling.
@@ -56,17 +55,17 @@ AI_ROLE = f"""
 
         Example 1:
         Conversation history:
-        {SALESPERSON_NAME}: Hey, good morning! <END_OF_TURN>
+        {SALESPERSON_NAME}: Hey, good morning! 
         User: Hello, who is this? <END_OF_TURN>
         {SALESPERSON_NAME}: This is {SALESPERSON_NAME} calling from {COMPANY_NAME}. How are you? 
-        User: I am well, why are you calling? <END_OF_TURN>
-        {SALESPERSON_NAME}: I am calling to talk about options for your home insurance. <END_OF_TURN>
-        User: I am not interested, thanks. <END_OF_TURN>
-        {SALESPERSON_NAME}: Alright, no worries, have a good day! <END_OF_TURN> <END_OF_CALL>
+        User: I am well, why are you calling?
+        {SALESPERSON_NAME}: I am calling to talk about options for your home insurance. 
+        User: I am not interested, thanks. 
+        {SALESPERSON_NAME}: Alright, no worries, have a good day!
         End of example 1.
 
         You must respond according to the previous conversation history and the stage of the conversation you are at.
-        Only generate one response at a time and act as {SALESPERSON_NAME} only! When you are done generating, end with '<END_OF_TURN>' to give the user a chance to respond.
+        Only generate one response at a time and act as {SALESPERSON_NAME} only! When you are done generating, end with to give the user a chance to respond.
 """# Create an instance of the ProfitPilot class
 
 # Setup Clarifai with LangChain
@@ -110,13 +109,11 @@ for msg in st.session_state.messages:
 
 if user_prompt:
     st.session_state.messages.append({"role": "user", "content": user_prompt})
-    create_memory("conversation", user_prompt, metadata={"role": "user"})
 
     response = llm_chain.run(user_prompt)  # If you want to use llm_chain
 
     msg = {"role": AI_ROLE, "content": response}
     st.session_state.messages.append(msg)
-    create_memory("conversation", response, metadata={"role": AI_ROLE})
 
     with st.chat_message(AI_ROLE):
         st.write(msg["content"])
